68a3628c660a2f4007e4fcf83c58a273363c6a8e
cortiz642 <chris.o.ortiz93@gmail.com>
2022-05-16 17:00:19 -0500

    working on console IO exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,10 @@
+public class ConsoleExercises {
+//    double pi = 3.14159;
+    public static void main(String[] args) {
+        String name = "The value of pi is approximately";
+        double pi = 3.14159;
+        double almostPi = (int) pi;
+        System.out.printf("%s,%s", name,almostPi);
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -16,16 +16,57 @@ import javax.swing.*;
     *3.naming
      */
 //}
-
-public class HelloWorld {
-    public static void main(String[] args) {
+//Exercises Syntax,Types,and Variables q1-4
+public class HelloWorld { //line:20 assigns a class of HelloWorld
+    public static void main(String[] args) { //
     int myFavoriteNumber = 9;
     System.out.println(myFavoriteNumber);
 //    String myString = "Java";
 //    System.out.println('myString');
         String myString = "3.14159";
         System.out.println(myString);
+        //******************************************************************//
+        //**Q5.Declare an long variable named myNumber, but do not assign anything
+        // to it. Next try to print out myNumber to the console. What happens?**
+        //******************************************************************//
 
+        //Shows variable "myNumber" is not initialized meaning "myNumber variable
+        //needs a value in order to run
+//        long myNumber;
+//        System.out.println(myNumber);
 
+        //******************************************************************//
+        //Q6.Change your code to assign the value 3.14 to myNumber. What do you notice?
+        //******************************************************************//
+        //Will not run because it is assigned the incorrect "Data Type" should
+        //be "double" instead of "long"
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+
+        //******************************************************************//
+        //Q7.Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+        //******************************************************************//
+        //Prints out 123 without the L
+        //The "L" is a suffix which tells the compiler that we have a long number literal
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+        //******************************************************************//
+        //Q8.Change your code to assign the value 123 to myNumber.
+        //
+        //Why does assigning the value 3.14 to a variable declared as a long
+        // not compile, but assigning an integer value does?
+        //******************************************************************//
+        //Prints out 123
+        /*long myNumber = 123;*/
+//        System.out.println(myNumber);
+
+        //******************************************************************//
+        //Q9.Change your code to declare myNumber as a float. Assign the value
+        // 3.14 to it. What happens? What are two ways we could fix this?
+        //******************************************************************//
+
+        //******************************************************************//
+        //******************************************************************//
     }
 }
