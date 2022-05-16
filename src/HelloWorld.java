a761bdae19477153d8462b3cd4ebf495f89807f5
cortiz642 <chris.o.ortiz93@gmail.com>
2022-05-16 13:55:45 -0500

    completed q1-4 within Java Syntax,Types, and Variables exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,31 @@
+import javax.swing.*;
+
+//public class HelloWorld {
+//    public static void main(String[] args) {
+////        System.out.println("Hello, World!");
+//          System.out.print("Hello, World!");
+//          System.out.print("Hello, \"\t\n\rChris\" World!");
+//
+//    }
+
+
+    /*
+    *this is a JTable
+    *1.something
+    *2.however
+    *3.naming
+     */
+//}
+
 public class HelloWorld {
     public static void main(String[] args) {
-//        System.out.println("Hello, World!");
-          System.out.print("Hello, World!");
-          System.out.print("Hello, \"Chris\" World!");
+    int myFavoriteNumber = 9;
+    System.out.println(myFavoriteNumber);
+//    String myString = "Java";
+//    System.out.println('myString');
+        String myString = "3.14159";
+        System.out.println(myString);
+
+
     }
 }
