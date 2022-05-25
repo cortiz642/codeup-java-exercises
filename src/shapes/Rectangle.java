1d67849a3df1fba40a459272d9776e5292368e9a
cortiz642 <chris.o.ortiz93@gmail.com>
2022-05-25 14:22:29 -0500

    working on inheritance and polymorphism

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class Rectangle {
+    protected double length;
+    protected double width;
+        public Rectangle(double length, double width){
+            this.length = length;
+            this.width = width;
+    }
+//        public int getArea(){
+//           return area = length * width;
+//        }
+//        public int getPerimeter(){
+//           return perimeter = 2 * length + 2 * width;
+//        }
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,7 @@
+package shapes;
+
+public class Square extends Rectangle{
+    public Square(double length, double width) {
+        super(length, width);
+    }
+}
