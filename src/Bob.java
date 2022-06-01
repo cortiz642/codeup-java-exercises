eb78888cb3021e26164766b4a01ba26d87070c39
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-01 09:19:15 -0500

    added bob exercises, extension from String

---
+++ src/Bob.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hey?");
+        String userInput = sc.next();
+        System.out.println("Who?");
+//        String userInput = sc.next();
+
+    }
+}
