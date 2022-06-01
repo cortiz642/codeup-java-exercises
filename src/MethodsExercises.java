eeb229ab6e339126e9c46c36d56c03814c2b9685
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-01 09:26:10 -0500

    added to class

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,36 +1,36 @@
 public class MethodsExercises {
     public static void main(String[] args) {
 
-        //*****************************Q1.a***********************************//
+
         //Basic Arithmetic
         //
         //Create four separate methods. Each will perform an arithmetic operation:
         //
         //Addition
-        System.out.println(Addition(9,9));
+        System.out.println(Addition(9, 9));
         //Subtraction
-        System.out.println(Subtraction(9,9));
+        System.out.println(Subtraction(9, 9));
         //Multiplication
-        System.out.println(Multiplication(9,9));
+        System.out.println(Multiplication(9, 9));
         //Division
-        System.out.println(Division(9,9));
+        System.out.println(Division(9, 9));
 
-        //*****************************Q1.a***********************************//
+
     }
+
     public static int Addition(int a, int b) {
         return a + b;
     }
-    public static int Subtraction(int a, int b){
+
+    public static int Subtraction(int a, int b) {
         return a - b;
     }
-    public static int Multiplication(int a, int b){
+
+    public static int Multiplication(int a, int b) {
         return a * b;
     }
-    public static int Division(int a, int b){
+
+    public static int Division(int a, int b) {
         return a / b;
     }
-    //*****************************Q1.b***********************************//
-    //Each function needs to take two parameters/arguments so that the operation can be performed.
-
-    //*****************************Q1.b***********************************//
 }
