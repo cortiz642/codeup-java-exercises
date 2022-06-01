e86d51beea7ee2498c474fb0c4cec5dac92e143a
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-01 09:14:47 -0500

    added exercises

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,8 +1,19 @@
 public class StringExercise {
 //     public class TernaryTester {
     public static void main(String[] args) {
-                     int x = 5;
-                    System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7);
+        String education = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(education);
+
+        String check = "Check this out!, s inside of s!";
+        System.out.println(check);
+
+        String windows = "In windows, the main drive is usually C:";
+        System.out.println(windows);
+
+        String back = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(back);
                  }
  }
 
