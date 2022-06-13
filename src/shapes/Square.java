3e44cb8001a7306d25a312e977b812523918dd03
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-13 00:20:27 -0500

    added overrides

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,28 +2,51 @@
 package shapes;
 //Created a class named Square inside of shapes
 //which extends Rectangle
-public class Square extends Rectangle{
-    protected double side;
-        public Square(double side) {
-        super(side, side);
-        this.side = side;
-
+public class Square extends Quadrilateral{
+    public Square(double length, double width) {
+        super(length, width);
     }
-    //seperate instance of setclass
-        @Override
-        public double getArea(){
-            return side * side;
-        }
 
-    //seperate instance of setclass
-        @Override
-        public double getPerimeter(){
-            return 4 * side;
-            }
-
-    public double getSide() {
-            return 8 * side;
+    @Override
+    public double getPerimeter() {
+        return length + length + width + width;
     }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public void setLength(double length) {
+    this.length = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+    this.width = width;
+    }
+//    protected double side;
+//        public Square(double side) {
+//        super(side, side);
+//        this.side = side;
+//
+//    }
+//    //seperate instance of setclass
+//        @Override
+//        public double getArea(){
+//            return side * side;
+//        }
+//
+//    //seperate instance of setclass
+//        @Override
+//        public double getPerimeter(){
+//            return 4 * side;
+//            }
+//
+//    public double getSide() {
+//            return 8 * side;
+//    }
 
 //    public int getSide() {
 //        return 0;
