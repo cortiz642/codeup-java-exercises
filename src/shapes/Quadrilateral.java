94b75408fea8dc7db219316800f855eb0b9c3f2a
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-14 22:59:18 -0500

    added quadrilateral class

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,2 +1,19 @@
-package shapes;public class Quadrilateral {
+package shapes;
+
+public abstract class Quadrilateral extends Shape implements Measurable{
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+    public double getLength(double length){
+        return length;
+    }
+    public double getWidth(double width){
+        return width;
+    }
+    public abstract void setLength(double length);
+    public abstract void setWidth(double width);
 }
