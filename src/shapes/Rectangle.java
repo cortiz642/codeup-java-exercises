40568aacf7edb1c6dd64fe3f4fd1c30c8a43f2bc
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-14 23:00:42 -0500

    added overrides and extended quadrilateral

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,25 +1,48 @@
 package shapes;
 
 //Created a class named "Rectangle"
-public class Rectangle {
-    //Protected properties for length and width
-    protected double length;
-    protected double width;
-    //defined Rectangle Constructor
-    //used double for length and width to accept two numbers
-        public Rectangle(double length, double width){
-            this.length = length;
-            this.width = width;
+public class Rectangle extends Quadrilateral implements Measurable{
+    public Rectangle(double length, double width) {
+        super(length, width);
     }
 
+    @Override
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
 
-    //Created getArea method
-        public double getArea(){
-            return width * length;
-        }
-        //Created getPerimeter method
-        public double getPerimeter(){
-           return 2 * (length + width);
-        }
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public void setLength(double length) {
+    this.length = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+    this.width = width;
+    }
+    //Protected properties for length and width
+//    protected double length;
+//    protected double width;
+//    //defined Rectangle Constructor
+//    //used double for length and width to accept two numbers
+//        public Rectangle(double length, double width){
+//            this.length = length;
+//            this.width = width;
+//    }
+//
+//
+//    //Created getArea method
+//        public double getArea(){
+//            return width * length;
+//        }
+//        //Created getPerimeter method
+//        public double getPerimeter(){
+//           return 2 * (length + width);
+//        }
 
 }
