b3a74cb1889f5ed48c4314bea1cad5d09d4808b4
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-16 00:15:27 -0500

    revised exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -42,11 +42,15 @@ public class Input {
         return scanner.nextInt();
     }
     public double getDouble(double min, double max){
+        Scanner sc = new Scanner(System.in);
         Double myMinDoubleValue = Double.MIN_VALUE;
         Double myMaxDoubleValue = Double.MAX_VALUE;
+//        Scanner sc = new Scanner(System.in);
         System.out.println("Double minimum value = " + myMinDoubleValue);
+        String user = scanner.nextLine();
         System.out.println("Double maximum value = " + myMaxDoubleValue);
         return scanner.nextDouble();
+
     }
     public Input() {
         this.scanner = new Scanner(System.in);
@@ -58,7 +62,9 @@ public class Input {
         Input yesNo = new Input();
         yesNo.yesNo();
         Input getInt = new Input();
-        getInt.getInt(10, 30);
+        getInt.getInt();
+        Input getDouble = new Input();
+        getDouble.getDouble(5.5,10.5);
     }
     }
 
