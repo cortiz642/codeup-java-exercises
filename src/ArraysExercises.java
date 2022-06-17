e905e598e449b6c450a99007ace18f81e4ea843c
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-17 10:54:45 -0500

    arrays exercise completed

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,11 +1,15 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
-    //Create a class inside of src named ArraysExercises. Create a main
-    // method for this class to do your work.
+
     //What happens when you run the following code? Why is Arrays.toString necessary?
     //When running without adding ".toString method" you will get an error because
     //the array needs to be converted to a string
+    public static Person[] addPerson(Person[]people, Person person){
+        Person[] newPersons = Arrays.copyOf(people, people.length + 1);
+        newPersons[newPersons.length -1] = person;
+        return newPersons;
+    }
     public static void main(String[]args){
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
@@ -13,19 +17,26 @@ public class ArraysExercises {
     //Create an array that holds 3 Person objects. Assign a new instance of
         // the Person class to each element. Iterate through the array and print
         // out the name of each person in the array.
-
-        String[] Person = new String[3];//Array holds 3 Person objects
-
+        Person[] people = new Person[3];//Array holds 3 Person objects
         //Assigned new instance of the Person class to each element.
 //       Person = new String[3];
-        Person[0]="juan";
-        Person[1]="julio";
-        Person[2]="carl";
+        people[0] = new Person("juan");
+        people[1] = new Person("julio");
+        people[2] = new Person("carl");
+
+        Person person = new Person("julia");
+        Person[] people1 = addPerson(people, person);
 
         //Iterated through the array
-        for(String perObj: Person){
+        for(Person perObj: people1){
         //Prints out name of each person within the array
-            System.out.println(perObj);
+            System.out.println(perObj.getName());
+
+//            public static String addPerson(String[]){
+}
+            }
+
+
         }
 
     //Create a static method named addPerson. It should accept an array of
@@ -38,5 +49,5 @@ public class ArraysExercises {
 //        static String addPerson(String[] Person= new String[0]);
 
         //
-    }
-}
+
+
