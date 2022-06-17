c186860c3cef71dbe4a111047209d60defeb309b
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-17 13:27:56 -0500

    exercise completed

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,14 @@
 public class ServerNameGenerator {
+    public static String[] adjectives = {"accomplished", "adaptable", "adventurous", "affectionate", "amazing", "amicable", "ambitious", "articulate", "careful", "cheerful"};
+
+    public static String[] nouns = {"answer", "battery", "army", "breakfast", "joy", "optimism", "confidence", "strength", "respect", "leadership"};
+
+    public static String random(String[] arr){
+        return arr[(int)Math.floor((Math.random() * (arr.length -1)))];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Here is your server name:");
+        System.out.println(random(adjectives) + " " + random(nouns));
+    }
 }
