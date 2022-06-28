b305a9602e950209613b868a0a04430accb57f51
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-27 21:34:01 -0500

    java review

---
+++ src/assessmentReview.java
@@ -0,0 +1,11 @@
+public class assessmentReview {
+
+}
+
+
+
+
+
+//1a. Make a class called review and create a public static method that takes
+//        in a double called radius, so that you can find the circumference of a
+//        circle. (Circumference Equation: C = 2πr)
