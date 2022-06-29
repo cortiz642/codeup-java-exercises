c4c03abe886c2f6ab9efc8e62d49bace1eb93e56
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-28 22:40:15 -0500

    carfields practice java

--- src/carfields/Main.java
+++ src/carfields/Main.java
@@ -1,2 +1,10 @@
-package carfields;public class Main {
+package carfields;
+
+public class Main {
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
+    }
 }
